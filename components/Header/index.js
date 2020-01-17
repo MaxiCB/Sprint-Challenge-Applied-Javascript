0293fec1f5823e8ddfa35b9defa5307d16a84922
MaxiCB <aaroncb@aaroncb.com>
2020-01-17 10:30:46 -0600

    ADD Header
    - Implemented Header functionality
    - Implemented formatted date string
    - Implemented header div
    - Implemented header span
    - Implemented header h1

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+const headerContainer = document.querySelector('.header-container');
+
+let header = Header();
+
+headerContainer.appendChild(header);
+
+function Header() {
+
+    let dateString = (new Date()).toString().split(' ').splice(1,3).join(' ');
+
+    let header = document.createElement('div');
+    header.classList.add('header');
+
+    let span = document.createElement('span');
+    span.classList.add('date')
+    span.textContent = `${dateString.toUpperCase()}`
+
+    let h1 = document.createElement('h1');
+    h1.textContent = 'Lambda Times';
+
+    let span2 = document.createElement('span');
+    span2.classList.add('temp');
+    span2.textContent = '98°';
+
+    header.appendChild(span);
+    header.appendChild(h1);
+    header.appendChild(span2);
+
+    return header;
+}
