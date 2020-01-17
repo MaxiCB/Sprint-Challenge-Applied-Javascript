aa81b252a29f23fd62dec641b21ebd78a16517e4
MaxiCB <aaroncb@aaroncb.com>
2020-01-17 11:28:58 -0600

    ADD Carousel
    - Implemented carousel functionality
    - Carousel iterates over images fom assets/carousel
    - Added Carousel script to index.html

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,68 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let images = [
+  'computer.jpeg',
+  'mountains.jpeg',
+  'trees.jpeg',
+  'turntable.jpeg'
+]
+
+const carElement = document.querySelector('.carousel-container');
+
+carElement.appendChild(createCarousel(images));
+
+function createCarousel(images){
+  let carouselDiv = document.createElement('div');
+  carouselDiv.classList.add('carousel');
+
+  let leftButton = document.createElement('div');
+  leftButton.classList.add('left-button');
+  leftButton.textContent = ' < ';
+
+  let rightButton = document.createElement('div');
+  rightButton.classList.add('right-button');
+  rightButton.click(function() {nextSlide(1)})
+  rightButton.textContent = ' > ';
+
+  carouselDiv.appendChild(leftButton);
+
+  images.forEach((image) => {
+    let imagesElem = document.createElement('img');
+    imagesElem.classList.add("slides")
+    imagesElem.src = `../../assets/carousel/${image}`;
+    carouselDiv.appendChild(imagesElem);
+  })
+
+  carouselDiv.appendChild(rightButton);
+
+  return carouselDiv;
+}
+
+var index = 0;
+showSlides(index);
+// nextSlide(index);
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  index++;
+  if (index > slides.length) {index = 1}
+  slides[index-1].style.display = "block";
+  setTimeout(showSlides, 4000);
+}
+
+function nextSlide(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  if (n > slides.length) {index = 1}
+  if (n < 1) {index = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[index-1].style.display = "block";
+}
